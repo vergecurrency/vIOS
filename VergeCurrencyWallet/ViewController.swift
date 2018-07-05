b90b8e2b042e953db2367fa689f7aa4adedc944a
Swen van Zanten <swen@troublefree.nl>
2018-07-06 01:43:06 +0200

    Initial Commit

---
+++ VergeCurrencyWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VergeCurrencyWallet
+//
+//  Created by Swen van Zanten on 06-07-18.
+//  Copyright © 2018 Verge Currency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
