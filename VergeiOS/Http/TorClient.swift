1a8042761b2c72822dc3851cfca5e1f18df77a1c
Swen van Zanten <swen@troublefree.nl>
2018-07-27 00:14:31 +0200

    Tor starts! 💆‍♀️🚀

--- VergeiOS/Http/TorClient.swift
+++ VergeiOS/Http/TorClient.swift
@@ -11,48 +11,75 @@ import NetworkExtension
 import Tor
 
 class TorClient {
-    func startt() {
-        let dataDirectory = URL(string: NSTemporaryDirectory())
-        let controlSocket = dataDirectory?.appendingPathComponent("control_port")
-        
-        let controller = TorController(socketURL: controlSocket!)
-        controller.addObserver(forCircuitEstablished: { (established) in
-            print(established)
-        })
-        
-        do {
-            try controller.connect()
-        } catch {
-            print("Failed")
-        }
-    }
-    
     func start() {
-        let config = TorConfiguration()
-        config.cookieAuthentication = true
-        config.dataDirectory = URL(string: NSTemporaryDirectory())
-        config.controlSocket = config.dataDirectory?.appendingPathComponent("control_port")
-        config.arguments = ["--allow-missing-torrc", "--ignore-missing-torrc"]
-        
+        // Get the tor configuration.
+        let config = self.createTorConfiguration()
+
+        // Start a tor thread.
         let thread = TorThread(configuration: config)
         thread.start()
-        
+
         let cookieUrl = config.dataDirectory?.appendingPathComponent("control_auth_cookie")
+
         do {
             let cookie = try Data(contentsOf: cookieUrl!)
             let torController = TorController(socketURL: config.controlSocket!)
+            self.addControllerObserver(torController)
 
             torController.authenticate(with: cookie) { (success, error) in
-                print(success)
-                print("hello")
+                if (!success) {
+                    return
+                }
 
-                torController.addObserver(forCircuitEstablished: { established in
-                    print("established")
-                    print(established)
-                })
+                print(torController.isConnected)
+                self.addControllerObserver(torController)
             }
         } catch {
-            print("Failed")
+            print("Tor Failed at starting 🤷‍♀️")
+        }
+    }
+    
+    private func createTorConfiguration() -> TorConfiguration {
+        let config = TorConfiguration()
+        config.cookieAuthentication = true
+        config.dataDirectory = URL(fileURLWithPath: self.createTorDirectory())
+        config.controlSocket = config.dataDirectory?.appendingPathComponent("control_port")
+        config.arguments = ["--allow-missing-torrc", "--ignore-missing-torrc"]
+        
+        return config
+    }
+    
+    private func createTorDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        let torPath = "\(documentsDirectory)/Tor"
+        
+        do {
+            try FileManager.default.createDirectory(atPath: torPath, withIntermediateDirectories: false, attributes: [
+                FileAttributeKey.posixPermissions: 0o700
+            ])
+        } catch {
+            print("Directory previously created. 🤷‍♀️")
+        }
+        
+        return torPath
+    }
+    
+    private func addControllerObserver(_ torController: TorController) {
+        torController.addObserver(forCircuitEstablished: { established in
+            if (!established) {
+                return
+            }
+            print("established")
+            self.startUrlSession(torController)
+        })
+    }
+    
+    private func startUrlSession(_ torController: TorController) {
+        torController.getSessionConfiguration() { sessionConfig in
+            print("sessionConfig")
+            let session = URLSession(configuration: sessionConfig!)
+            print(session.dataTask(with: URL(fileURLWithPath: "http://google.com")))
         }
     }
 }
