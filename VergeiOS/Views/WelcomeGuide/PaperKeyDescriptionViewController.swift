fcdaee791b4fdb06d5a11f52f4f37a64edb8ce71
Swen van Zanten <swen@troublefree.nl>
2018-07-29 10:08:16 +0200

    Added the words page and the summary page.. still wip

--- VergeiOS/Views/WelcomeGuide/PaperKeyDescriptionViewController.swift
+++ VergeiOS/Views/WelcomeGuide/PaperKeyDescriptionViewController.swift
@@ -13,19 +13,18 @@ class PaperKeyDescriptionViewController: UIViewController {
     @IBOutlet weak var paperKeyIcon: UIImageView!
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
+        
         UIApplication.shared.statusBarStyle = .lightContent
         
         self.paperKeyIcon.alpha = 0.0
-        self.paperKeyIcon.center.y -= 60.0
+        self.paperKeyIcon.center.y -= 20.0
         
         UIView.animate(withDuration: 0.3, delay: 0.2, options: .curveEaseInOut, animations: {
             self.paperKeyIcon.alpha = 1.0
-            self.paperKeyIcon.center.y += 60.0
+            self.paperKeyIcon.center.y += 20.0
         }, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
     }
     
     // Dismiss the view
@@ -33,14 +32,16 @@ class PaperKeyDescriptionViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let backItem = UIBarButtonItem()
+        backItem.title = "Back"
+        backItem.tintColor = .white
+        navigationItem.backBarButtonItem = backItem
     }
-    */
 
 }
---
+++ VergeiOS/Views/WelcomeGuide/PaperKeyWordsViewController.swift
@@ -0,0 +1,120 @@
+//
+//  PaperKeyWordsViewController.swift
+//  VergeiOS
+//
+//  Created by Swen van Zanten on 29-07-18.
+//  Copyright © 2018 Verge Currency. All rights reserved.
+//
+
+import UIKit
+
+class PaperKeyWordsViewController: UIViewController {
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var progressionLabel: UILabel!
+    @IBOutlet weak var previousButton: RoundedButton!
+    @IBOutlet weak var nextButton: RoundedButton!
+    
+    var words: [String] = [
+        "words",
+        "coming",
+        "from",
+        "the",
+        "insight",
+        "api",
+        "verge",
+        "is",
+        "freking",
+        "awesome",
+        "wen",
+        "moon"
+    ]
+    var selectedWord = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.wordLabel.text = self.words.first
+        
+        self.updateView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
+    }
+    
+    @IBAction func previousWord(_ sender: Any) {
+        self.selectedWord -= 1
+        self.wordLabel.text = self.words[self.selectedWord]
+        
+        self.updateView()
+    }
+    
+    @IBAction func nextWord(_ sender: Any) {
+        if (self.selectedWord == (self.words.count - 1)) {
+            return self.performSegue(withIdentifier: "confirmPaperkey", sender: self)
+        }
+        
+        self.selectedWord += 1
+        self.wordLabel.text = self.words[self.selectedWord]
+        
+        self.updateView()
+    }
+    
+    func showDoneButton() {
+        self.nextButton.setTitle("Done", for: .normal)
+        self.nextButton.setImage(UIImage(named: "ClipboardCheck"), for: .normal)
+    }
+    
+    func showNextButton() {
+        self.nextButton.setTitle("Next", for: .normal)
+        self.nextButton.setImage(UIImage(named: "ArrowRight"), for: .normal)
+    }
+    
+    func updateView() {
+        self.progressionLabel.text = "\(self.selectedWord + 1) out of \(self.words.count)"
+        
+        if (self.selectedWord == 0) {
+            self.hideButton(self.previousButton)
+        }
+        
+        if (self.selectedWord > 0) {
+            self.showButton(self.previousButton)
+        }
+        
+        if (self.selectedWord == (self.words.count - 1)) {
+            self.showDoneButton()
+        } else {
+            self.showNextButton()
+        }
+    }
+    
+    func hideButton(_ button: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            button.alpha = 0.5
+            button.isEnabled = false
+        }
+    }
+    
+    func showButton(_ button: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            button.alpha = 1
+            button.isEnabled = true
+        }
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        let backItem = UIBarButtonItem()
+        backItem.title = "Back"
+        backItem.tintColor = .white
+        navigationItem.backBarButtonItem = backItem
+    }
+
+}
