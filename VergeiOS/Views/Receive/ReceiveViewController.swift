a5ccb2b38abcc2928bfa32cc7da48edcf194a8a7
Swen van Zanten <info@hdsign.nl>
2018-11-04 13:05:03 +0100

    Add initial design changes

--- VergeiOS/Views/Receive/ReceiveViewController.swift
+++ VergeiOS/Views/Receive/ReceiveViewController.swift
@@ -9,32 +9,84 @@
 import UIKit
 import QRCode
 
+@IBDesignable
 class ReceiveViewController: UIViewController {
-    
+
+    @IBOutlet weak var xvgCardContainer: UIView!
+    @IBOutlet weak var xvgCardImageView: UIImageView!
     @IBOutlet weak var qrCodeImageView: UIImageView!
+    @IBOutlet weak var qrCodeContainerView: UIView!
+    @IBOutlet weak var cardAddress: UILabel!
+
+    @IBOutlet weak var addressTextField: SelectorButton!
+    @IBOutlet weak var amountTextField: SelectorButton!
+    @IBOutlet weak var stealthSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var qrCode = QRCode("cBQFLyaV5o17Rpx424JGHbQjdffyQs7Atm")
-        qrCode?.color = CIColor(cgColor: UIColor.primaryDark().cgColor)
-        self.qrCodeImageView.image = (qrCode?.image)!
+
+        let address = "1NMcHbiZuUYU9su1BDKTRtJvxnq14Z8YiT"
+
+        qrCodeContainerView.layer.cornerRadius = 10.0
+        qrCodeContainerView.clipsToBounds = true
+
+        cardAddress.text = address
+
+        addTapRecognizer(target: addressTextField, action: #selector(copyAddress(recognizer:)))
+        addTapRecognizer(target: xvgCardImageView, action: #selector(copyAddress(recognizer:)))
+
+        DispatchQueue.main.async {
+            self.addressTextField.valueLabel?.text = address
+            self.amountTextField.valueLabel?.text = NSNumber(value: 0.0).toXvgCurrency(fractDigits: 6)
+            self.createQRCode(address: address, stealth: self.stealthSwitch.isOn)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.xvgCardContainer.alpha = 0.0
+        self.xvgCardContainer.center.y += 20.0
+
+        UIView.animate(withDuration: 0.3, delay: 0.2, options: .curveEaseInOut, animations: {
+            self.xvgCardContainer.alpha = 1.0
+            self.xvgCardContainer.center.y -= 20.0
+        }, completion: nil)
     }
-    
 
-    /*
-    // MARK: - Navigation
+    func createQRCode(address: String, stealth: Bool = false) {
+        var qrCode = QRCode(address)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if stealth {
+            qrCode?.color = CIColor(cgColor: UIColor.backgroundBlue().cgColor)
+            qrCode?.backgroundColor = CIColor(cgColor: UIColor.primaryDark().cgColor)
+        } else {
+            qrCode?.color = .white
+            qrCode?.backgroundColor = CIColor(cgColor: UIColor(red: 0.11, green: 0.62, blue: 0.83, alpha: 1.0).cgColor)
+        }
+
+        qrCodeImageView.image = (qrCode?.image)!
     }
-    */
 
+    @IBAction func switchStealth(_ sender: UISwitch) {
+        createQRCode(address: cardAddress.text!, stealth: sender.isOn)
+
+        if sender.isOn {
+            xvgCardImageView.image = UIImage(named: "StealthReceiveCard")
+        } else {
+            xvgCardImageView.image = UIImage(named: "ReceiveCard")
+        }
+    }
+
+    func addTapRecognizer(target: UIView, action: Selector) {
+        let gesture = UITapGestureRecognizer(target: self, action: action)
+        gesture.numberOfTapsRequired = 2
+
+        target.addGestureRecognizer(gesture)
+    }
+
+    @objc func copyAddress(recognizer: UIGestureRecognizer) {
+        UIPasteboard.general.string = cardAddress.text
+        NotificationManager.shared.showMessage("Address copied!", duration: 3)
+    }
 }
