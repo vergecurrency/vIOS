eb1511b95d314719605013b65237411c86c09a17
Swen van Zanten <info@hdsign.nl>
2018-11-21 00:52:08 +0100

    Add bitcoinkit framework

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-brew install automake autoconf libtool gettext libevent && \
+brew install automake autoconf libtool gettext libevent openssl && \
 echo "Installing Tor dependency" && \
 carthage update --platform iOS
