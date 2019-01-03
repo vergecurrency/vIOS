fcaecdb9b87fb47a97c025751be47dd0b30489c8
Swen van Zanten <swen@troublefree.nl>
2019-01-03 13:16:43 +0100

    Release 0.13

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 brew install automake autoconf libtool gettext libevent openssl && \
+ln -s /usr/local/bin/glibtoolize /usr/local/bin/libtoolize && \
 echo "Installing Tor dependency" && \
 carthage update --platform iOS
