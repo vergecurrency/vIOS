44737f8e42db17b1b2acdbcbc08ef549a2e1829a
Swen van Zanten <info@hdsign.nl>
2019-01-27 17:26:50 +0100

    Skip carthage building when cache is available

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 brew install automake autoconf libtool gettext libevent openssl && \
 ln -s /usr/local/bin/glibtoolize /usr/local/bin/libtoolize && \
 echo "Installing Tor dependency" && \
-carthage update --platform iOS
+carthage update --platform iOS --cache-builds
