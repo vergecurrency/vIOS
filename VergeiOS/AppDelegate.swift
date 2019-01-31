85d1db1c43baa1bed26c511adf273153ae4f9b67
hellc <ivanmanov@live.com>
2019-02-01 02:03:12 +0300

    siri receive funds intent
    Allow to use Siri to present last generated address card image

--- VergeiOS/AppDelegate.swift
+++ VergeiOS/AppDelegate.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
-import Intents
 import CoreData
 import CoreStore
 import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var sendRequest: SendTransaction?
     var backgroundTaskIdentifier: UIBackgroundTaskIdentifier?
@@ -27,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         registerAppforDetectLockState()
         setupListeners()
-
+        
         // Start the tor client
         TorClient.shared.start {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
@@ -36,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         WatchSyncManager.shared.startSession()
-
+        
         do {
             CoreStore.defaultStack = DataStack(
                 xcodeModelName: "CoreData",
@@ -46,14 +45,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     "VergeiOS 2"
                 ]
             )
-
+            
             try CoreStore.addStorageAndWait(
                 SQLiteStore(fileName: "VergeiOS.sqlite", localStorageOptions: .allowSynchronousLightweightMigration)
             )
         } catch {
             print(error.localizedDescription)
         }
-
+        
         return true
     }
     
@@ -72,22 +71,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         WalletTicker.shared.stop()
         PriceTicker.shared.stop()
         TorClient.shared.resign()
-
+        
         showPinUnlockViewController(application)
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         
@@ -98,11 +97,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
@@ -112,20 +111,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         PriceTicker.shared.stop()
         TorClient.shared.resign()
     }
-
+    
     func torClientStarted() {
         // Start the price ticker.
         PriceTicker.shared.start()
-
+        
         if !ApplicationManager.default.setup {
             return
         }
-
+        
         // Start the wallet ticker.
         WalletTicker.shared.start()
         
         if #available(iOS 12.0, *) {
-            self.donateSiriIntents()
+            IntentsManager.donateIntents()
         }
     }
     
@@ -135,7 +134,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 TorClient.shared.resign()
             }
         }
-
+        
         CFNotificationCenterAddObserver(
             CFNotificationCenterGetDarwinNotifyCenter(),
             nil,
@@ -145,12 +144,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             .deliverImmediately
         )
     }
-
+    
     func showPinUnlockViewController(_ application: UIApplication) {
         if !ApplicationManager.default.setup {
             return
         }
-
+        
         if var topController = application.keyWindow?.rootViewController {
             while let presentedViewController = topController.presentedViewController {
                 topController = presentedViewController
@@ -167,24 +166,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             print("Show unlock view")
             topController.present(vc, animated: false, completion: nil)
-        }
-    }
-    
-    @available(iOS 12.0, *)
-    func donateSiriIntents() {
-        let priceIntent = VergePriceIntent()
-        priceIntent.suggestedInvocationPhrase = "Verge price"
-        
-        let interaction = INInteraction(intent: priceIntent, response: nil)
-        
-        interaction.donate { (error) in
-            if error != nil {
-                if let error = error as NSError? {
-                    NSLog("Interaction donation failed: %@", error)
-                } else {
-                    NSLog("Successfully donated interaction")
-                }
-            }
         }
     }
 }
