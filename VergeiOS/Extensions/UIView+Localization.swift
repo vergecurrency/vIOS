67c69f32192c94de70365b55b952883ead839af0
Ivan Manov <ivanmanov@live.com>
2019-04-20 18:27:47 +0500

    Localizable strings base. Partial localization

---
+++ VergeiOS/Extensions/UIView+Localization.swift
@@ -0,0 +1,67 @@
+//
+//  UILabel+Localization.swift
+//  VergeiOS
+//
+//  Created by Ivan Manov on 20/04/2019.
+//  Copyright © 2019 Verge Currency. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    @IBInspectable var localizationId: String {
+        set(value) {
+            self.text = NSLocalizedString(value, comment: "")
+        }
+        get {
+            return ""
+        }
+    }
+    
+}
+
+extension UIButton {
+    
+    @IBInspectable var localizationId: String {
+        set(value) {
+            self.setTitle(NSLocalizedString(value, comment: ""), for: .normal)
+        }
+        get {
+            return ""
+        }
+    }
+    
+}
+
+extension UINavigationItem {
+    
+    @IBInspectable var localizationTitleId: String {
+        set(value) {
+            self.title = NSLocalizedString(value, comment: "")
+        }
+        get {
+            return ""
+        }
+    }
+    
+    @IBInspectable var localizationPromptId: String {
+        set(value) {
+            self.prompt = NSLocalizedString(value, comment: "")
+        }
+        get {
+            return ""
+        }
+    }
+    
+    @IBInspectable var localizationBackButtonId: String {
+        set(value) {
+            self.backBarButtonItem?.title = NSLocalizedString(value, comment: "")
+        }
+        get {
+            return ""
+        }
+    }
+    
+}
