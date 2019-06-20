4c5293e5267600f40a3db9f026456d6338e7d311
Ivan Manov <ivanmanov@live.com>
2019-06-20 11:55:27 +0300

    PinUnlock as inactivity view

--- VergeiOS/AppDelegate.swift
+++ VergeiOS/AppDelegate.swift
@@ -13,6 +13,8 @@ import SwinjectStoryboard
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    static var inactivityView = PinUnlockViewController.createFromStoryBoard().view!
+    
     var application: Application?
     var window: UIWindow?
     var sendRequest: TransactionFactory?
@@ -85,12 +87,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // Stop fiat rate ticker.
         Application.container.resolve(FiatRateTicker.self)?.stop()
+        
+        // Show inactivity view
+        AppDelegate.inactivityView.frame = UIScreen.main.bounds
+        self.window?.addSubview(AppDelegate.inactivityView)
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         Application.container.resolve(TorClient.self)?.restart()
         
+        // Remove inactivity view
+        AppDelegate.inactivityView.removeFromSuperview()
+        
+        // Show pincode
         showPinUnlockViewController(application)
     }
     
