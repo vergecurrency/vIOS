8074a7c2817a173c8ee6509df488990d089ef3e0
Swen van Zanten <info@hdsign.nl>
2019-07-27 12:28:15 +0200

    WIP

--- VergeiOS/Components/Themes/UIView+Themeable.swift
+++ VergeiOS/Components/Themes/UIView+Themeable.swift
@@ -139,7 +139,7 @@ extension UIPageControl {
 
     override func updateColors() {
         self.currentPageIndicatorTintColor = ThemeManager.shared.primaryLight()
-        self.pageIndicatorTintColor = ThemeManager.shared.secondaryLight()
+        self.pageIndicatorTintColor = ThemeManager.shared.vergeGrey()
     }
 }
 
